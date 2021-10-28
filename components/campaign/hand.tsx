ae37f9d2847ba9fcae033c5fa4ca8941393979e8
Hayden Faulds <fauldsh@gmail.com>
2021-10-28 11:43:55 +0100

    improve hand animations

--- components/campaign/hand.tsx
+++ components/campaign/hand.tsx
@@ -6,55 +6,52 @@ export default function Hand(props) {
   const [selected, setSelected] = useState(null);
 
   const select = (card) => {
-    setCards(cards.filter((c) => c != card))
+    if (selected == card) {
+      setSelected(null)
+      return
+    }
+    setHover(null)
     setSelected(card)
   }
 
-  const deselect = (card) => {
-    setCards(cards.concat([card]).sort((a,b) => a.id.localeCompare(b.id)))
-    setSelected(null)
-  }
-
   return <>
-    { selected && (
-	<div
-	  className="absolute inline-block border-solid border-4 bg-white shadow w-40 h-52 border-gray-300"
-	  style={{
-	    transform: "scale(1.2) translate(-5rem, -5rem)",
-	    zIndex: "2",
-	  }}
-	  onClick={() => deselect(selected)}
-	>
-	  <p className="font-bold"> Card { selected.id }</p>
-	</div>
-    )}
     {
       cards.map((card, i) => {
-	const ri = i - Math.floor(cards.length/2) + ((1 - (cards.length % 2)) * 0.5)
-	const x = (ri-1) * 5
-	const y = Math.abs(1 * ri)
-	const rot = 10 * ri
-	return <div
-	  key={card.id}
-	  className="absolute inline-block border-solid border-4 bg-white shadow w-40 h-52 hover:border-gray-300"
-	  style={{
-	    transform: `translate(${x}rem, ${y}rem) rotate(${rot}deg)`,
-	      transition: "transform 50ms",
-	      ... (hover == card ? {
-		zIndex: "1",
-		transform: `scale(1.1) translate(${x}rem, ${y - 1.5}rem) rotate(${rot}deg)`,
-	      } : {})
-	  }}
-	  onClick={() => select(card) }
-	  onMouseOver={()=> !selected && setHover(card) }
-	  onMouseOut={()=> !selected && setHover(null) }
-	  >
-	    <p className="font-bold"> Card { card.id }</p>
-	    <p> adfasdfasdfasdf </p>
-	    <p> adfasdfasdfasdf </p>
-	    <p> adfasdfasdfasdf </p>
-	    <p> adfasdfasdfasdf </p>
-	  </div>
+        const unselectedCards = cards.length - (!!selected ? 1 : 0)
+        const pos = card.id.localeCompare(selected?.id) < 0 ? i : i - 1
+        // position relative to center
+        const relpos = pos - Math.floor(unselectedCards/2) + ((1 - (unselectedCards % 2)) * 0.5)
+        // move by 5 rem on x axis per position
+        const x = (relpos-1) * 5
+        // move by 1 rem in an arc on y axis, so that the middle card is highest
+        const y = Math.abs(1 * relpos)
+        // rotate by 10
+        const rot = 10 * relpos
+        return <div
+          key={card.id}
+          className="absolute inline-block border-solid border-4 bg-white shadow w-40 h-52 hover:border-gray-300"
+          style={{
+            transform: `translate(${x}rem, ${y}rem) rotate(${rot}deg)`,
+            transition: "transform 200ms",
+            ... (hover == card && {
+              zIndex: "1",
+              transform: `scale(1.1) translate(${x}rem, ${y - 1.5}rem) rotate(${rot}deg)`,
+            }),
+            ... (selected == card && {
+              zIndex: "2",
+              transform: "scale(1.2) translate(-5rem, -5rem)",
+            })
+          }}
+          onClick={() => select(card) }
+          onMouseOver={()=> setHover(card) }
+          onMouseOut={()=> setHover(null) }
+        >
+          <p className="font-bold"> Card { card.id }</p>
+          <p> adfasdfasdfasdf </p>
+          <p> adfasdfasdfasdf </p>
+          <p> adfasdfasdfasdf </p>
+          <p> adfasdfasdfasdf </p>
+        </div>
       })
     }
   </>
