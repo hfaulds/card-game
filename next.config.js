3b51ff6eba4b783d22a9f3464edaa98b166b5caa
Hayden Faulds <fauldsh@gmail.com>
2021-10-30 13:20:42 +0100

    use swc rather than babel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const { withSuperjson } = require('next-superjson')
+
+module.exports = withSuperjson()({
   env: {
     NEXTAUTH_URL: `https://${process.env.VERCEL_URL}`
   },
   images: {
     domains: ['lh3.googleusercontent.com']
   }
-};
+});
