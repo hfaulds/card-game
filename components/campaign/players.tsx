0c57575ebc639fd5e1da45af4c74b4e17336d493
Hayden Faulds <fauldsh@gmail.com>
2021-10-31 21:41:16 +0000

    change how hands are customised

--- components/campaign/players.tsx
+++ components/campaign/players.tsx
@@ -9,43 +9,47 @@ export default function Players(props) {
   })
 
   const addCard = (userCampaign, card) => {
-    const oldCards = state.users[userCampaign.id]?.cards || []
+    const oldCards = state.users[userCampaign.id]?.cards || {}
     setState({
       ...state,
       ...{
         users: {
           ...state.users,
           [userCampaign.id]: {
-            cards: oldCards.concat(card),
+            cards: {
+              ...oldCards,
+              [card.id]: (oldCards[card.id] || 0) + 1,
+            },
           },
         },
       },
     })
   }
+
   return props.campaign.users
     .filter((campaignUser) => !!campaignUser.accepted)
     .map((userCampaign) => {
       const user = userCampaign.user
       const userCards = state.users[userCampaign.id]?.cards || []
       return (
-        <div key={userCampaign.id} className="flex space-x-5">
+        <div key={userCampaign.id} className="flex flex-wrap space-x-5">
           <div className="">{user.name}</div>
-
-          {userCards.map((card, i) => (
+          {Cards.map((card) => (
             <div
-              key={i}
-              className="flex justify-center inline-block border-solid border-2 shadow w-20 h-24 bg-white border-gray-400 text-gray-400"
+              key={card.id}
+              className="border-solid border-2 mb-2 shadow w-28 h-40 bg-white border-gray-400 text-gray-400"
             >
-              {card.name}
+              <div>{card.name}</div>
+              <div>{userCards[card.id] || 0}</div>
+
+              <div
+                onClick={() => addCard(userCampaign, card)}
+                className="text-gray-400 hover:text-gray-600"
+              >
+                <PlusCircleIcon className="block w-6 h-6 stroke-1" />
+              </div>
             </div>
           ))}
-
-          <div
-            onClick={() => addCard(userCampaign, Cards[0])}
-            className="flex justify-center inline-block border-solid border-2 shadow w-20 h-24 pt-4 bg-white hover:bg-gray-200 border-gray-400 hover:border-gray-600 text-gray-400 hover:text-gray-600"
-          >
-            <PlusCircleIcon className="block w-12 h-12 stroke-1" />
-          </div>
         </div>
       )
     })
