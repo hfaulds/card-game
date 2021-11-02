fb3e6591319c95f8805dc662a9a688e9ec7fd548
Hayden Faulds <fauldsh@gmail.com>
2021-11-02 09:38:07 +0000

    add prettier but more js-heavy card rendering

--- components/campaign/hand.tsx
+++ components/campaign/hand.tsx
@@ -1,55 +1,68 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 export default function Hand(props) {
   const [cards, setCards] = useState(props.cards)
   const [hover, setHover] = useState(null)
-  const [selected, setSelected] = useState<null | { id: String }>(null)
+  const [selected, setSelected] = useState<null | any>(null)
+  const [width, setWidth] = useState(0)
+  const ref = useRef<HTMLDivElement>(null);
 
-  const select = (card) => {
-    if (selected == card) {
+  useEffect(() => {
+    window.addEventListener('resize', () => setWidth(ref?.current?.offsetWidth || 0))
+    setWidth(ref?.current?.offsetWidth || 0)
+  })
+
+  const select = (card, i) => {
+    if (selected?.instanceId == card.instanceId) {
       setSelected(null)
       return
     }
     setHover(null)
-    setSelected(card)
+    setSelected({...card, pos: i})
+  }
+
+  const getTransform = (pos) => {
+    const unselectedCards = cards.length - (!!selected ? 1 : 0)
+    // position relative to center adjusted for selected card
+    const relpos =
+      (pos > selected?.pos ? pos-1 : pos) -
+      Math.floor(unselectedCards / 2) +
+      (1 - (unselectedCards % 2)) * 0.5
+    // distribute evenly across x axis
+    const x = relpos * (width / cards.length) * 0.8
+    // find y position on curve
+    // adjust x to raise curve
+    const y = (Math.pow(x * 0.8, 2) / width)
+    // rotate to align with normal of curve
+    const rot = Math.atan(y / x) * (180/Math.PI)
+    return { x, y, rot }
   }
 
   return (
-    <>
+    <div ref={ref} className="w-8/12 mx-auto px-4 sm:px-6">
       {cards.map((card, i) => {
-        const unselectedCards = cards.length - (!!selected ? 1 : 0)
-        const pos =
-          card.id.localeCompare(selected && selected.id) < 0 ? i : i - 1
-        // position relative to center
-        const relpos =
-          pos -
-          Math.floor(unselectedCards / 2) +
-          (1 - (unselectedCards % 2)) * 0.5
-        // move by 5 rem on x axis per position
-        const x = (relpos - 1) * 5
-        // move by 1 rem in an arc on y axis, so that the middle card is highest
-        const y = Math.abs(1 * relpos)
-        // rotate by 10
-        const rot = 10 * relpos
+        const { x, y, rot } = getTransform(i)
         return (
           <div
-            key={card.id}
+            key={card.instanceId}
             className="absolute inline-block border-solid border-4 bg-white shadow w-40 h-52 hover:border-gray-300"
             style={{
-              transform: `translate(${x}rem, ${y}rem) rotate(${rot}deg)`,
+              marginLeft: "-5rem",
+              transformOrigin: "50%",
+              transform: `translate(${x}px, ${y}px) rotate(${rot}deg)`,
               transition: "transform 200ms",
               ...(hover == card && {
                 zIndex: 1,
-                transform: `scale(1.1) translate(${x}rem, ${
+                transform: `scale(1.1) translate(${x}px, ${
                   y - 1.5
-                }rem) rotate(${rot}deg)`,
+                }px) rotate(${rot}deg)`,
               }),
-              ...(selected == card && {
+              ...(selected?.instanceId == card.instanceId && {
                 zIndex: 2,
-                transform: "scale(1.2) translate(-5rem, -5rem)",
+                transform: "scale(1.2) translate(0rem, -5rem)",
               }),
             }}
-            onClick={() => select(card)}
+            onClick={() => select(card, i)}
             onMouseOver={() => setHover(card)}
             onMouseOut={() => setHover(null)}
           >
@@ -61,6 +74,6 @@ export default function Hand(props) {
           </div>
         )
       })}
-    </>
+    </div>
   )
 }
--- pages/encounter/[id].tsx
+++ pages/encounter/[id].tsx
@@ -8,6 +8,7 @@ import { useState } from "react"
 import Hand from "components/campaign/hand"
 import Modal from "components/modal"
 import { Cards } from "lib/cards"
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Page(props) {
   const { data: session } = useSession()
@@ -32,73 +33,77 @@ export default function Page(props) {
     let c: any[] = []
     let card = Cards.find((card) => card.id == id)
     for (let i=0; i < (quantity as number); i++) {
-      c.push(card)
+      c.push({
+        instanceId: uuidv4(),
+        ...card,
+      })
     }
     return c
   })
 
   return (
-    <Layout
-      fullscreen={true}
-      breadcrumbs={[
-        {
-          text: props.campaign.name as string,
-          url: `/campaign/${props.campaign.id}`,
-        },
-        { text: props.encounter.name as string },
-      ]}
-    >
-      <div className="pb-4">
-        <h1 className="inline-block text-3xl mr-4">{props.campaign.name}</h1>
-        <button className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
-          <span
-            className="font-small"
-            onClick={() => setManageCharacterModal(true)}
-          >
-            {" "}
-            Manage Character{" "}
-          </span>
-        </button>
-      </div>
-
-      <div
-        className="h-4/6 w-full text-center bg-blue-100 flex"
-        style={{
-          backgroundImage:
-            "repeating-linear-gradient(#ccc 0 1px, transparent 1px 100%), repeating-linear-gradient(90deg, #ccc 0 1px, transparent 1px 100%)",
-          backgroundSize: "21px 21px",
-        }}
+    <>
+      <Layout
+        fullscreen={true}
+        breadcrumbs={[
+          {
+            text: props.campaign.name as string,
+            url: `/campaign/${props.campaign.id}`,
+          },
+          { text: props.encounter.name as string },
+        ]}
       >
-        <div className="flex-none w-30"></div>
-        <div className="flex-grow"></div>
-        <div className="flex-none w-30">
-          <div className="border-solid border-4 bg-white p-2">
-            <p className="font-bold mb-2"> Order </p>
-            {props.campaign.users
-              .filter((campaignUser) => !!campaignUser.accepted)
-              .map(({ user }) => (
-                <p key={user.id}> {user.name} </p>
-              ))}
-          </div>
-          {currentCampaignUser.admin && (
-            <button className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
-              <span className="font-small" onClick={endTurn}>
-                {" "}
-                Skip Turn{" "}
-              </span>
-            </button>
-          )}
+        <div className="pb-4">
+          <h1 className="inline-block text-3xl mr-4">{props.campaign.name}</h1>
+          <button className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
+            <span
+              className="font-small"
+              onClick={() => setManageCharacterModal(true)}
+            >
+              {" "}
+              Manage Character{" "}
+            </span>
+          </button>
         </div>
-      </div>
 
-      <div className="fixed bottom-0 h-60 w-8/12 text-center">
+        <div
+          className="h-4/6 w-full text-center bg-blue-100 flex"
+          style={{
+            backgroundImage:
+              "repeating-linear-gradient(#ccc 0 1px, transparent 1px 100%), repeating-linear-gradient(90deg, #ccc 0 1px, transparent 1px 100%)",
+            backgroundSize: "21px 21px",
+          }}
+        >
+          <div className="flex-none w-30"></div>
+          <div className="flex-grow"></div>
+          <div className="flex-none w-30">
+            <div className="border-solid border-4 bg-white p-2">
+              <p className="font-bold mb-2"> Order </p>
+              {props.campaign.users
+                .filter((campaignUser) => !!campaignUser.accepted)
+                .map(({ user }) => (
+                  <p key={user.id}> {user.name} </p>
+                ))}
+            </div>
+            {currentCampaignUser.admin && (
+              <button className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
+                <span className="font-small" onClick={endTurn}>
+                  {" "}
+                  Skip Turn{" "}
+                </span>
+              </button>
+            )}
+          </div>
+        </div>
+
+        {manageCharacterModal && (
+          <Modal hide={() => setManageCharacterModal(false)}>Hi</Modal>
+        )}
+      </Layout>
+      <div className="fixed bottom-0 h-60 w-full text-center">
         <Hand cards={cards} playCard={playCard} />
       </div>
-
-      {manageCharacterModal && (
-        <Modal hide={() => setManageCharacterModal(false)}>Hi</Modal>
-      )}
-    </Layout>
+    </>
   )
 }
 
