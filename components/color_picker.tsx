79435e07773ad864d1aa3bdbe64b16723dbecebe
Hayden Faulds <fauldsh@gmail.com>
2021-11-05 13:23:39 +0000

    color picker nitpick

--- components/color_picker.tsx
+++ components/color_picker.tsx
@@ -20,7 +20,7 @@ export default function Picker(props) {
       </button>
       {open && (
         <div
-          className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          className="origin-top-right absolute right-0 mt-1 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu"
         >
           {colors.map((c) => (
